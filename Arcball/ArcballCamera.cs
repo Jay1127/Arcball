0ccfaefbc4c3e7697afbac62b365f042ad7d73c6
Jay <yeonjae5252@gmail.com>
2018-10-25 00:33:37 +0900

    Upload acrball camera project

---
+++ Arcball/ArcballCamera.cs
@@ -0,0 +1,158 @@
+﻿using Jay.Geometry.Transformation;
+using Jay.Geometry.Util;
+using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcball
+{
+    /// <summary>
+    /// Arcball camera representation
+    /// </summary>
+    public class ArcballCamera
+    {
+        /// <summary>
+        /// camera view matrix
+        /// </summary>
+        public Matrix4 ViewMatrix => Scaling * Rotation * Translation;
+
+        /// <summary>
+        /// center of sphere
+        /// </summary>
+        public Vector3 Center { get; private set; }
+
+        /// <summary>
+        /// radius of sphere
+        /// </summary>
+        public float Radius { get; private set; }
+
+        /// <summary>
+        /// translation matrix
+        /// </summary>
+        public Matrix4 Translation { get; set; }
+
+        /// <summary>
+        /// rotation matrix
+        /// </summary>
+        public Matrix4 Rotation { get; set; }
+
+        /// <summary>
+        /// scaling matrix
+        /// </summary>
+        public Matrix4 Scaling { get; set; }
+
+        /// <summary>
+        /// screen's width
+        /// </summary>
+        public int Width { get; set; }
+
+        /// <summary>
+        /// screen's height
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// pan sensitivity
+        /// (to be changed)
+        /// </summary>
+        private readonly float _panSensitivity = 5;
+
+        /// <summary>
+        /// rotation sensitivity
+        /// </summary>
+        private readonly float _rotateSensitivity = 2;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="center">center of sphere</param>
+        /// <param name="radius">radius of sphere</param>
+        /// <param name="viewWidth">screen's width</param>
+        /// <param name="viewHeight">screen's height</param>
+        public ArcballCamera(Vector3 center, float radius, int viewWidth, int viewHeight)
+        {
+            this.Center = center;
+            this.Radius = radius;
+            this.Width = viewWidth;
+            this.Height = viewHeight;
+
+            InitDefaultPosition();
+        }
+
+        /// <summary>
+        /// Calculate dafualt camera position(simple case)
+        /// </summary>
+        public void InitDefaultPosition()
+        {
+            var position = new Vector3(Center.X, Center.Y, Center.Z + Radius);
+            Translation = Matrix4.LookAt(position, Center, Vector3.UnitY);
+            Rotation = Matrix4.Identity;
+            Scaling = Matrix4.Identity;
+        }
+
+        /// <summary>
+        /// Pan camera
+        /// </summary>
+        /// <param name="dv">movement vector</param>
+        public void Pan(Vector3 dv)
+        {
+            var translation = new Translation3D(dv / _panSensitivity);
+            Translation *= translation.Matrix;
+        }
+
+        /// <summary>
+        /// Rotate camera by pivot(pivot is sphere center)
+        /// </summary>
+        /// <param name="scrStartPoint">screen point(mouse start)</param>
+        /// <param name="srcEndPoint"></param>
+        public void Rotate(Vector2 scrStartPoint, Vector2 srcEndPoint)
+        {
+            Vector3 arcStartPoint = MapSphereCoordinate(scrStartPoint);
+            Vector3 arcEndPoint = MapSphereCoordinate(srcEndPoint);
+
+            Vector3 rotateAxis = Vector3.Cross(arcStartPoint, arcEndPoint);
+            float angle = Vector3.CalculateAngle(arcStartPoint, arcEndPoint) * _rotateSensitivity;
+
+            // pivot 중심 회전
+             var rotation = new Rotation3D(MathUtil.ToDegrees(angle), rotateAxis, Center);
+            Rotation *= rotation.Matrix;
+        }
+
+        public void Zoom(float zoomFactor)
+        {            
+            zoomFactor = zoomFactor > 0 ? 1.1f : 0.9f;
+
+            var scaling = new Scaling3D(zoomFactor, Center);
+            Scaling *= scaling.Matrix;
+        }
+
+        private Vector3 MapSphereCoordinate(Vector2 pt)
+        {
+            Vector3 coord = new Vector3();
+
+            coord.X = (2 * pt.X - Width) / Width;
+            coord.Y = -(2 * pt.Y - Height) / Height;            // 좌표축 반대라서 대칭
+
+            coord = Vector3.Clamp(coord, new Vector3(-1, -1, 0), new Vector3(1, 1, 0));
+
+            float length_squared = coord.X * coord.X + coord.Y * coord.Y;
+
+            if (length_squared <= 1.0)
+            {
+                coord.Z = (float)Math.Sqrt(1.0 - length_squared);
+            }
+            else
+            {
+                // checking
+                coord = Vector3.Normalize(coord);
+                //coord *= (float)(1.0 / Math.Sqrt(length_squared));
+                coord.Z = 0.0f;                
+            }
+
+            return coord;
+        }
+    }
+}
---
+++ Arcball/Sample/Scene.cs
@@ -0,0 +1,141 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcball
+{
+    class Scene : GameWindow
+    {
+        ArcballCamera _camera;
+
+        int _vao;
+        int _vbo;
+        int _ebo;
+
+        Vector3[] _vertices = Teapot.Vertices;
+        uint[] _indices = Teapot.Indices;
+
+        bool _isMousePosInScreen;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            // set arcball camera
+            CalculateSphereInfo(out Vector3 center, out float radius);
+            _camera = new ArcballCamera(center, radius, Width, Height);
+
+            // generate buffer of teapot model
+            GL.GenVertexArrays(1, out _vao);
+            GL.BindVertexArray(_vao);
+
+            GL.GenBuffers(1, out _vbo);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, Vector3.SizeInBytes * _vertices.Length, _vertices, BufferUsageHint.StaticDraw);
+
+            GL.GenBuffers(1, out _ebo);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _ebo);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, sizeof(int) * _indices.Length, _indices, BufferUsageHint.StaticDraw);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+        }
+
+        protected override void OnMouseMove(MouseMoveEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            if (!_isMousePosInScreen) return;
+
+            if(e.Mouse.LeftButton == ButtonState.Pressed)
+            {
+                var prevPos = new Vector2(e.Position.X - e.XDelta, e.Position.Y - e.YDelta);
+                var currPos = new Vector2(e.Position.X, e.Position.Y);
+
+                _camera.Rotate(prevPos, currPos);
+            }
+            else if(e.Mouse.RightButton == ButtonState.Pressed)
+            {
+                _camera.Pan(new Vector3(e.XDelta, -e.YDelta, 0));
+            }
+        }
+
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            base.OnMouseWheel(e);
+            if (!_isMousePosInScreen) return;
+
+            _camera.Zoom(e.Delta);
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            _isMousePosInScreen = true;
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+            _isMousePosInScreen = false;
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            int viewVolumn = 100;
+            GL.Viewport(0, 0, Width, Height);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.Ortho(-viewVolumn, viewVolumn, -viewVolumn, viewVolumn, -viewVolumn, viewVolumn);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.ClearColor(Color.Black);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            var viewMatrix = _camera.ViewMatrix;
+            GL.LoadMatrix(ref viewMatrix);
+
+            GL.BindVertexArray(_vao);
+            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            // unbind
+            GL.BindVertexArray(0);
+
+            SwapBuffers();
+        }
+
+        /// <summary>
+        /// Calculate sphere's center, radius
+        /// </summary>
+        /// <param name="center">center of sphere</param>
+        /// <param name="radius">radius of shpere</param>
+        private void CalculateSphereInfo(out Vector3 center, out float radius)
+        {
+            var maxX = _vertices.Max(v => v.X);
+            var maxY = _vertices.Max(v => v.Y);
+            var maxZ = _vertices.Max(v => v.Z);
+            var minX = _vertices.Min(v => v.X);
+            var minY = _vertices.Min(v => v.Y);
+            var minZ = _vertices.Min(v => v.Z);
+
+            center = new Vector3((maxX + minX) / 2, (maxY + minY) / 2, (maxZ + minZ) / 2);
+            radius = Vector3.Distance(new Vector3(maxX, maxY, maxZ), center) * 2;
+        }
+    }
+}
